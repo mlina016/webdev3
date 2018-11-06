5b61d2c1a179270eee68ecd053083542491c9f66
Lucas Mlinar <mlina016@ad.umn.edu>
2018-11-06 10:46:06 -0600

    Created new symbology and did fixes

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 // create request for GeoJSON
 var request = new Promise(function (resolve, reject) {
 	var request = new XMLHttpRequest();
-	request.addEventListener("load", function(){ resolve(this.responseText) });
-	request.open("GET", "data/duluthprecinctsWGS84.geojson");
+	request.addEventListener("load", function(){ resolve(this.responseText)});
+	request.open("GET", "data/duluth_precincts_WGS84.geojson");
 	request.send();
 });
 
@@ -41,28 +41,45 @@ request.then(function(values){
 		    	// console.log('thirdPartyVote:', thirdPartyVote);
 		    	console.log('pct3rd:', thirdPartyPct);
 
+                var twoPartyVote = demVote+repVote
+                
+                var repPer = Math.round((repVote/totalVote)*100)
+                var demPer = Math.round((demVote/totalVote)*100)
+                console.log('percent',demPer);
 		    	// assign colors from the ColorBrewer yellow-green scale
 		    	var fill;
 		    	// equal interval classification
-		    	// 7% or less
-		    	if (thirdPartyPct <= 7) {
-		    		fill = '#f7fcb9';
+		    	// 70% or more
+		    	if (demPer >= 70) {
+		    		fill = '#0571b0';
 		    	}
-		    	// 11% or less
-		    	else if (thirdPartyPct <= 11) {
-		    		fill = '#addd8e';
+		    	//65% or more
+		    	else if (demPer >= 65) {
+		    		fill = '#92c5de';
 		    	}
-		    	// 12% or more
-		    	else {
-		    		fill = '#31a354';
+		    	//60% or more
+		    	else if (demPer >= 60) {
+		    		fill = '#f7f7f7';
 		    	}
+                //55% or more
+                else if (demPer >= 55){
+                    fill = '#f4a582';
+                }
+                //50% or more
+                else if (demPer >= 50) {
+                    fill = '#ca0020';
+                }
+                //everything else
+                else {
+                    fill = '#31a354';
+                }
 
 		    	// FIXME: remove existing symbolization and create a diverging
 		    	// symbology based the democrat or republic vote as a percent
 		    	// of the total vote.
 		    	// Use Colorbrewer's 5-class Red-Blue colors for this
 		    	// http://colorbrewer2.org/#type=diverging&scheme=RdBu&n=5
-
+                
 		    	// return style specification
 		        return {
 		        	color: '#636363',
@@ -91,7 +108,7 @@ request.then(function(values){
 		    var html = "<h4>Precinct: "+pctName+"</h4>"+
 		    	"<table><tr><td>Democratic votes: </td><td>"+demVote+"</td></tr>"+
 		    	"<tr><td>Republican votes: </td><td>"+repVote+"</td></tr>"+
-		    	"<tr><td>Tomfoolery: </td><td>"+balderdash+"</td></tr>"+
+		    	"<tr><td>Total Votes: </td><td>"+totalVote+"</td></tr>"+
 		    	"<tr><td>Third party votes: </td><td>"+(totalVote-demVote-repVote)+"</td></tr></table>";
 
 		    return html;
